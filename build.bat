c0d2fad09f729df4051f38d48abf7139124f5310
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-10 17:52:05 -0300

    Batch: compilacion con ghdl

---
+++ build.bat
@@ -0,0 +1,8 @@
+set build_target = "processor_tb"
+set build_dependencies = "alu.vhd memory.vhd processor.vhd processor_tb.vhd registers.vhd"
+set build_options = "--ieee=synopsys -fexplicit"
+set simulation_options = "--assert-level=none --ieee-asserts=disable-at-0 --stop-time=1ms"
+ghdl --clean
+ghdl -a --ieee=synopsys -fexplicit alu.vhd memory.vhd processor.vhd processor_tb.vhd registers.vhd
+ghdl -e --ieee=synopsys -fexplicit processor_tb
+ghdl -r --ieee=synopsys -fexplicit processor_tb --assert-level=none --ieee-asserts=disable-at-0 --stop-time=1ms --vcd=processor_tb.vcd
