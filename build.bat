f25fb7ec10be7c313d73572d7c6ccd07c1af296d
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-18 20:06:23 -0300

    Build: por fin lo pude arreglar

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
-set build_target = "processor_tb"
-set build_dependencies = "alu.vhd memory.vhd processor.vhd processor_tb.vhd registers.vhd"
-set build_options = "--ieee=synopsys -fexplicit"
-set simulation_options = "--assert-level=none --ieee-asserts=disable-at-0 --stop-time=1ms"
+set build_target=processor_tb
+set build_dependencies=alu.vhd memory.vhd processor.vhd processor_tb.vhd registers.vhd
+set build_options=--ieee=synopsys -fexplicit
+set simulation_options=--assert-level=none --ieee-asserts=disable-at-0 --stop-time=1ms
 ghdl --clean
-ghdl -a --ieee=synopsys -fexplicit alu.vhd memory.vhd processor.vhd processor_tb.vhd registers.vhd unidad_forwarding.vhd
-ghdl -e --ieee=synopsys -fexplicit processor_tb
-ghdl -r --ieee=synopsys -fexplicit processor_tb --assert-level=none --ieee-asserts=disable-at-0 --stop-time=1ms --vcd=processor_tb.vcd
+ghdl -a %build_options% %build_dependencies%
+ghdl -e %build_options% %build_target% 
+ghdl -r %build_options% %build_target% %simulation_options% --vcd=%build_target%.vcd
