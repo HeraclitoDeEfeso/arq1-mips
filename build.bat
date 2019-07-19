13fbc2ea3cc45ee5ea7a6aa41f0cbca7d7294b01
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2019-07-19 12:18:22 -0300

    Build: faltaba la unidad de forwarding

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 set build_target=processor_tb
-set build_dependencies=alu.vhd memory.vhd processor.vhd processor_tb.vhd registers.vhd
+set build_dependencies=alu.vhd memory.vhd processor.vhd processor_tb.vhd registers.vhd unidad_forwarding.vhd
 set build_options=--ieee=synopsys -fexplicit
 set simulation_options=--assert-level=none --ieee-asserts=disable-at-0 --stop-time=1ms
 ghdl --clean
